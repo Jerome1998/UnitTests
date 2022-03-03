25d536345014745258f1a1367ba6ec91323a0a58
Jerome Holetzke <jerome.holetzke@inter.ikea.com>
2022-03-03 13:49:37 +0100

    added AutoFixtureExample

---
+++ UnitTests.Tests/Mocking/AutoFixtureExample.cs
@@ -0,0 +1,55 @@
+﻿using AutoFixture;
+using FakeItEasy;
+using NUnit.Framework;
+using UnitTests.Fundamentals;
+
+namespace UnitTests.Tests.Mocking
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        private CustomerController controller;
+        private Fake<ICustomerRepository> customerRepositiory;
+
+        [SetUp]
+        public void Setup()
+        {
+            customerRepositiory = new Fake<ICustomerRepository>();
+            controller = new CustomerController(customerRepositiory.FakedObject);
+        }
+
+        [Test]
+        public void GetCstomer_CustomerExists_ReturnsOkWithCustomer()
+        {
+            var fixture = new Fixture();
+            var customer = fixture.Create<Customer>();
+            customerRepositiory.CallsTo(x => x.GetCustomer(A<int>.Ignored)).Returns(customer);
+
+            var result = controller.GetCustomer(42);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+            Assert.That((result as Ok).Data, Is.TypeOf<Customer>());
+        }
+
+        [Test]
+        public void GetCustomer_CustomerNotExists_ReturnsNotFound()
+        {
+            customerRepositiory.CallsTo(x => x.GetCustomer(A<int>.Ignored)).Returns(null);
+
+            var result = controller.GetCustomer(42);
+
+            Assert.That(result, Is.TypeOf<NotFound>());
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(-100)]
+        public void GetCustomer_IdOutOfRange_ReturnsNotFound(int id)
+        {
+            var result = controller.GetCustomer(id);
+
+            Assert.That(result, Is.TypeOf<NotFound>());
+        }
+    }
+}
--- UnitTests/Fundamentals/CustomerController.cs
+++ UnitTests/Fundamentals/CustomerController.cs
@@ -2,18 +2,52 @@
 {
     public class CustomerController
     {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
         public ActionResult GetCustomer(int id)
         {
-            if (id == 0)
+            if (id <= 0)
                 return new NotFound();
-            
-            return new Ok();
-        }        
+
+            var customer = _customerRepository.GetCustomer(id);
+
+            if (customer is null)
+                return new NotFound();
+
+            return new Ok(customer);
+        }
+    }
+
+    public interface ICustomerRepository
+    {
+        Customer GetCustomer(int id);
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
     
     public class ActionResult { }
     
     public class NotFound : ActionResult { }
  
-    public class Ok : ActionResult { }
+    public class Ok : ActionResult 
+    {
+        public Ok()
+        { }
+
+        public Ok(object data)
+        {
+            this.Data = data;
+        }
+
+        public object Data { get; }
+    }
 }
